b14546b0a3f45a14ac50d04d04fccd50b263fc6e
Bartosz Mucha <muchzill4@gmail.com>
2013-12-14 21:40:12 +0000

    Dotfiles installer basepath fix

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-me=`basename $0`
-for file in `ls -A | grep -v $me`; do
-  ln -fs "$PWD/$file" $HOME
+me=$(basename $0)
+my_dir=$(cd "$(dirname "$0")"; pwd)
+
+for file in $(ls -A $my_dir | grep -v $me); do
+  ln -fs "$my_dir/$file" $HOME
 done
