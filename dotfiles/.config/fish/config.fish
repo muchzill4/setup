be3ce9d510db8c10816b07dcbf0da5f439afb684
Bartosz Mucha <muchzill4@gmail.com>
2013-12-29 20:14:03 +0000

    Custom fish title

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -7,6 +7,15 @@ set fish_color_valid_path normal --underline
 set fish_color_param normal
 set fish_color_search_match --background=6d5d2b
 
+# only cwd if process is fish
+function fish_title
+    if [ $_ = 'fish' ]
+        echo (prompt_pwd)
+    else
+        echo $_
+    end
+end
+
 # include local fish config
 set -l local_config "$HOME/.config/fish/local_config.fish"
 test -e $local_config; and source $local_config
