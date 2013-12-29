9e2de1036fa434fc2b19264aadab4b178fc8deae
Bartosz Mucha <muchzill4@gmail.com>
2013-12-29 19:56:26 +0000

    Fish colors

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,6 +1,12 @@
 # clear default greeting
 set fish_greeting
 
+# colors
+set fish_color_normal white
+set fish_color_valid_path normal --underline
+set fish_color_param normal
+set fish_color_search_match --background=6d5d2b
+
 # include local fish config
 set -l local_config "$HOME/.config/fish/local_config.fish"
 test -e $local_config; and source $local_config
