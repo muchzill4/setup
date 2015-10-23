b7c0524ed19d8fdbe393cc3a9ee683f9824f4792
Bartek Mucha <muchzill4@gmail.com>
2015-10-23 21:07:05 +0100

    bundle exec alias

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -25,7 +25,8 @@ alias md 'mkdir -p'
 alias g 'git'
 alias brew-up 'brew update; and brew upgrade --all; and brew cleanup; and brew prune'
 alias serve 'ruby -run -e httpd . -p 8000'
-alias t 'tmux attach; or tmux -u new'
+alias t 'tmux attach -d; or tmux -u new'
+alias be 'bundle exec'
 
 # include local fish config
 set -l local_config "$HOME/.config/fish/local_config.fish"
