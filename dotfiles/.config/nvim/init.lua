e62025db34890541f09d09d5d4fb2e19dbbb555f
Bartek Mucha <muchzill4@gmail.com>
2021-01-28 17:02:32 +0000

    Consistent quotes

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -139,7 +139,7 @@ end
 -- }}} --
 
 -- LSP {{{ --
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
   }
