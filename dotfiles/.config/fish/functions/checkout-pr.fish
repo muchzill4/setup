395e400d9a48da58d3ab76ab1f7489c4abba6a00
Bartek Mucha <muchzill4@gmail.com>
2021-04-06 07:58:52 +0100

    Facepalm is an understatement

--- dotfiles/.config/fish/functions/checkout-pr.fish
+++ dotfiles/.config/fish/functions/checkout-pr.fish
@@ -1,5 +1,5 @@
 function checkout-pr -e "checkout selected pull request"
-    cat pulls.json |
+    gh api 'repos/:owner/:repo/pulls' |
     jq --raw-output '.[] | "#\(.number) \(.title)"' |
     fzf |
     sed -E 's/^#([0-9]+).*/\1/' |
