339f30a7d953c76f762d6124579e0836fafe6c70
Bartek Mucha <muchzill4@gmail.com>
2021-05-11 20:32:19 +0100

    Turns out it's builtin

--- dotfiles/.config/nvim/lua/mc4/telescope/init.lua
+++ dotfiles/.config/nvim/lua/mc4/telescope/init.lua
@@ -20,16 +20,6 @@ require('telescope').load_extension('fzy_native')
 
 local M = {}
 
-function M.git_branches()
-  require('telescope.builtin').git_branches {
-    attach_mappings = function(_, map)
-      map('i', '<c-d>', actions.git_delete_branch)
-      map('n', '<c-d>', actions.git_delete_branch)
-      return true
-    end
-  }
-end
-
 function M.edit_dotfiles()
   require('telescope.builtin').find_files {
     prompt_title = "~ dotfiles ~",
