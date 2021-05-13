db0c8890655e36222518b014b9bbcb5435053054
Bartek Mucha <muchzill4@gmail.com>
2021-05-13 07:18:30 +0100

    Too much

--- dotfiles/.config/nvim/lua/mc4/lsp.lua
+++ dotfiles/.config/nvim/lua/mc4/lsp.lua
@@ -3,7 +3,7 @@ local bmap = require('mc4.shortcuts').bmap
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- virtual_text = false,
+    virtual_text = false,
   }
 )
 
