2d085014efc1da9d21f287f50acd2e52582d58e3
Bartek Mucha <muchzill4@gmail.com>
2021-06-03 20:14:11 +0100

    No more "can't find session"

--- dotfiles/.config/fish/functions/t.fish
+++ dotfiles/.config/fish/functions/t.fish
@@ -9,7 +9,7 @@ function t -d "attach or run a tmux session from ~/Dev/*"
   if test -n "$selected"
     set -l dirname (basename $selected)
 
-    tmux switch-client -t $dirname
+    tmux switch-client -t $dirname 2> /dev/null
     if test $status -ne 0
       tmux new -c $selected -d -s $dirname && tmux switch-client -t $dirname ||
         tmux new -c $selected -A -s $dirname
