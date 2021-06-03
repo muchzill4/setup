87885d15e4f5230ed38444ec4bc1988ce71812d1
Bartek Mucha <muchzill4@gmail.com>
2021-06-03 20:07:38 +0100

    Fishier t

---
+++ dotfiles/.config/fish/functions/t.fish
@@ -0,0 +1,18 @@
+function t -d "attach or run a tmux session from ~/Dev/*"
+  set -l path $HOME/Dev
+  set -l selected (
+    find $path/* -maxdepth 1 -mindepth 1 -type d |
+      string replace "$HOME" "~" |
+      fzf-tmux -p
+  )
+
+  if test -n "$selected"
+    set -l dirname (basename $selected)
+
+    tmux switch-client -t $dirname
+    if test $status -ne 0
+      tmux new -c $selected -d -s $dirname && tmux switch-client -t $dirname ||
+        tmux new -c $selected -A -s $dirname
+    end
+  end
+end
