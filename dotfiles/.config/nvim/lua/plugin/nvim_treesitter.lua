586c7cc6c3cf36703b3fa495b4f51f510268e0f1
Bartek Mucha <muchzill4@gmail.com>
2021-06-03 08:57:14 +0100

    Treesitter ident

--- dotfiles/.config/nvim/lua/plugin/nvim_treesitter.lua
+++ dotfiles/.config/nvim/lua/plugin/nvim_treesitter.lua
@@ -2,5 +2,9 @@ require("nvim-treesitter.configs").setup {
   ensure_installed = "maintained",
   highlight = {
     enable = true
+  },
+  indent = {
+    enable = true,
+    disable = {'python'}
   }
 }
