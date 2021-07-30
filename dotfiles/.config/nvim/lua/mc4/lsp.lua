ff647183c543591c8b3310c90154df54a15f262e
Bartek Mucha <muchzill4@gmail.com>
2021-07-30 13:02:15 +0100

    Disable lsp signs

--- dotfiles/.config/nvim/lua/mc4/lsp.lua
+++ dotfiles/.config/nvim/lua/mc4/lsp.lua
@@ -9,6 +9,7 @@ local bmap = require("mc4.shortcuts").bmap
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
+    signs = false,
   }
 )
 
