d0cce7c3a016da724bce59277466e87226e9f245
Bartek Mucha <muchzill4@gmail.com>
2021-07-30 13:02:07 +0100

    Show relative path

--- dotfiles/.config/nvim/lua/plugin/lualine.lua
+++ dotfiles/.config/nvim/lua/plugin/lualine.lua
@@ -17,7 +17,7 @@ end
 lualine.setup({
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'filename'},
+    lualine_b = {{'filename', path = 1}},
     lualine_c = {},
     lualine_x = {
       {
@@ -34,7 +34,7 @@ lualine.setup({
   },
   inactive_sections = {
     lualine_a = {},
-    lualine_b = {{'filename'}},
+    lualine_b = {{'filename', path = 1}},
     lualine_c = {},
     lualine_x = {},
     lualine_y = {},
