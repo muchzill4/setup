d13d05b541971cb4c258df05af0ae7d4ce3314a0
Bartek Mucha <muchzill4@gmail.com>
2021-08-19 19:35:35 +0100

    Move telescope to plugins

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -5,5 +5,3 @@ require('mc4.options')
 require('mc4.shortcuts').source_plugins()
 
 require('mc4.lsp')
-require('mc4.telescope')
-require('mc4.telescope.maps')
--- dotfiles/nvim/.config/nvim/lua/plugin/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/telescope.lua
@@ -38,6 +38,19 @@ telescope.setup {
 }
 require("telescope").load_extension("fzf")
 
+
+local map = require("mc4.shortcuts").map
+
+map("n", "<Leader>ff", "<Cmd>Telescope find_files<CR>")
+map("n", "<Leader>fb", "<Cmd>Telescope buffers<CR>")
+map("n", "<Leader>ft", "<Cmd>Telescope tags<CR>")
+map("n", "<Leader>fs", "<Cmd>Telescope live_grep<CR>")
+map("n", "<Leader>fS", "<Cmd>Telescope grep_string<CR>")
+map("n", "<Leader>fx", "<Cmd>Telescope builtin<CR>")
+map("n", "<Leader>gb", "<Cmd>Telescope git_branches<CR>")
+map("n", "<Leader>fd", "<Cmd>lua require('plugin.telescope').find_dotfiles()<CR>")
+map("n", "<leader>ll", "<Cmd>Telescope lsp_document_diagnostics<CR>")
+
 local M = {}
 
 function M.find_dotfiles()
