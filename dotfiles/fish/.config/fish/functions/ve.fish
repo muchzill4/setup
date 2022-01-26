f7e868ad173f0d1f89180569e833e5344bfd81d7
Bartek Mucha <muchzill4@gmail.com>
2022-01-26 09:57:10 +0000

    Execute in venv easily

---
+++ dotfiles/fish/.config/fish/functions/ve.fish
@@ -0,0 +1,11 @@
+function ve -d "execute in python venv"
+  source .venv/bin/activate.fish
+  if count $argv > /dev/null
+    $argv
+    deactivate
+  else
+    echo "Empty \$argv"
+    deactivate
+    return 1
+  end
+end
