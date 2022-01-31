7a4c1980d9242da9d7a9d3ddbe511e62a5f361e9
Bartek Mucha <muchzill4@gmail.com>
2022-01-31 16:15:37 +0000

    Use new sunmeko

--- dotfiles/nvim/.config/nvim/lua/mc4/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/mc4/lsp.lua
@@ -82,32 +82,27 @@ lspconfig.jedi_language_server.setup {
 }
 
 local sumneko_root_path = vim.fn.expand "$HOME/Dev/vcs/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/macOS/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
 lspconfig.sumneko_lua.setup {
-  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+  cmd = { sumneko_binary },
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
     Lua = {
       runtime = {
-        -- Tell the language server which version of Lua you"re using (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
-        -- Setup your lua path
-        path = vim.split(package.path, ";"),
+        path = runtime_path
       },
       diagnostics = {
-        -- Get the language server to recognize the `vim` global
         globals = { "vim" },
       },
       workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-        },
+        library = vim.api.nvim_get_runtime_file("", true),
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
       },
