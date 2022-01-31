7ee7e3aa2f1d964d0c0aede75d1e5474a5c40963
Bartek Mucha <muchzill4@gmail.com>
2022-01-31 16:15:52 +0000

    Update cmp config (warning: c-d -> c-b)

--- dotfiles/nvim/.config/nvim/lua/plugin/cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/cmp.lua
@@ -22,22 +22,23 @@ local has_words_before = function()
 end
 
 cmp.setup {
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "path" },
-    { name = "snippy" },
-  },
   snippet = {
     expand = function(args)
       require("snippy").expand_snippet(args.body)
     end,
   },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "snippy" },
+  }, {
+    { name = "buffer" },
+    { name = "path" },
+  }),
   mapping = {
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = {
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
