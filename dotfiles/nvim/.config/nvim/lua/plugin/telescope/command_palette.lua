54ee575b80ace7f4992c8adb550c48322ced02ec
Bartek Mucha <muchzill4@gmail.com>
2022-04-26 22:50:45 +0200

    Make palette contents externally modifiable

--- dotfiles/nvim/.config/nvim/lua/plugin/telescope/command_palette.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/telescope/command_palette.lua
@@ -8,40 +8,6 @@ local action_state = require "telescope.actions.state"
 
 local last_selection = nil
 
-local palette = {
-  { name = "lsp rename", cmd = "lua vim.lsp.buf.rename()" },
-  { name = "lsp references", cmd = "Telescope lsp_references" },
-  { name = "lsp diagnostics", cmd = "Telescope diagnostics" },
-  { name = "test suite", cmd = "TestSuite", update_errmsg = true },
-  { name = "git diff", cmd = "Gdiffsplit" },
-  { name = "git push", cmd = "Git push" },
-  { name = "git push --force", cmd = "Git push --force" },
-  { name = "git browse", cmd = ".GBrowse" },
-  { name = "git branch", cmd = "Telescope git_branches" },
-  {
-    name = "dap continue",
-    cmd = "lua require('dap').continue()",
-  },
-  {
-    name = "dap toggle breakpoint",
-    cmd = "lua require('dap').toggle_breakpoint()",
-  },
-  {
-    name = "dap clear breakpoints",
-    cmd = "lua require('dap').clear_breakpoints()",
-  },
-  { name = "dap toggle repl", cmd = "lua require('dap').repl.toggle()" },
-  { name = "dap toggle ui", cmd = "lua require('dapui').toggle()" },
-  { name = "dap debug test", cmd = "lua require('dap-go').debug_test()" },
-  {
-    name = "dotfiles",
-    cmd = "lua require('plugin.telescope.find_dotfiles').find_dotfiles()",
-  },
-  { name = "telescope builtins", cmd = "Telescope" },
-  { name = "unload buffers", cmd = "%bd" },
-  { name = "help", cmd = "Telescope help_tags" },
-}
-
 local function feed_keys(keys)
   vim.api.nvim_input(vim.api.nvim_replace_termcodes(keys, true, false, true))
 end
@@ -54,12 +20,12 @@ local function run_cmd(cmd, update_errmsg)
   end
 end
 
-local function command_palette(opts)
+function M.command_palette(opts)
   opts = opts or {}
 
   local function finder()
     return finders.new_table {
-      results = palette,
+      results = require("plugin.telescope.palette").palette,
       entry_maker = function(entry)
         return {
           value = entry,
@@ -84,10 +50,6 @@ local function command_palette(opts)
       return true
     end,
   }):find()
-end
-
-function M.command_palette()
-  command_palette()
 end
 
 function M.command_palette_last_cmd()
---
+++ dotfiles/nvim/.config/nvim/lua/plugin/telescope/palette.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+M.palette = {
+  { name = "lsp rename", cmd = "lua vim.lsp.buf.rename()" },
+  { name = "lsp references", cmd = "Telescope lsp_references" },
+  { name = "lsp diagnostics", cmd = "Telescope diagnostics" },
+  { name = "test suite", cmd = "TestSuite", update_errmsg = true },
+  { name = "git diff", cmd = "Gdiffsplit" },
+  { name = "git push", cmd = "Git push" },
+  { name = "git push --force", cmd = "Git push --force" },
+  { name = "git browse", cmd = ".GBrowse" },
+  { name = "git branch", cmd = "Telescope git_branches" },
+  {
+    name = "dap continue",
+    cmd = "lua require('dap').continue()",
+  },
+  {
+    name = "dap toggle breakpoint",
+    cmd = "lua require('dap').toggle_breakpoint()",
+  },
+  {
+    name = "dap clear breakpoints",
+    cmd = "lua require('dap').clear_breakpoints()",
+  },
+  { name = "dap toggle repl", cmd = "lua require('dap').repl.toggle()" },
+  { name = "dap toggle ui", cmd = "lua require('dapui').toggle()" },
+  { name = "dap debug test", cmd = "lua require('dap-go').debug_test()" },
+  {
+    name = "dotfiles",
+    cmd = "lua require('plugin.telescope.find_dotfiles').find_dotfiles()",
+  },
+  { name = "telescope builtins", cmd = "Telescope" },
+  { name = "unload buffers", cmd = "%bd" },
+  { name = "help", cmd = "Telescope help_tags" },
+}
+
+function M.extend(palette)
+  for _, entry in ipairs(palette) do
+    table.insert(M.palette, entry)
+  end
+end
+
+return M
