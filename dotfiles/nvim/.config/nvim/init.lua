f2e42388557b2c02fe0f86f31b11ed7cd4960c92
Bartek Mucha <muchzill4@gmail.com>
2022-05-29 16:34:39 +0100

    Inline plugin sourcing

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -4,7 +4,13 @@ require "mc4.maps"
 require "mc4.plugins"
 require "mc4.options"
 require "mc4.statusline"
-
-require("mc4.shortcuts").source_plugins()
-
 require "mc4.lsp"
+
+for _, mod in ipairs(vim.api.nvim_get_runtime_file("lua/plugin/**/*.lua", true)) do
+  local ok, msg = pcall(loadfile(mod))
+
+  if not ok then
+    print("Failed to load: ", mod)
+    print("\t", msg)
+  end
+end
--- dotfiles/nvim/.config/nvim/lua/mc4/shortcuts.lua
+++ dotfiles/nvim/.config/nvim/lua/mc4/shortcuts.lua
@@ -18,18 +18,4 @@ function M.bmap(bufnr, mode, lhs, rhs, opts)
   vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
 end
 
--- https://github.com/tjdevries/astronauta.nvim
-function M.source_plugins()
-  for _, mod in
-    ipairs(vim.api.nvim_get_runtime_file("lua/plugin/**/*.lua", true))
-  do
-    local ok, msg = pcall(loadfile(mod))
-
-    if not ok then
-      print("Failed to load: ", mod)
-      print("\t", msg)
-    end
-  end
-end
-
 return M
