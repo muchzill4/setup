3d29ad0084a68a3f68dfa85578da374fad58e52d
Bartek Mucha <muchzill4@gmail.com>
2022-06-10 08:24:31 +0100

    Less verbosity = better failure visibility

--- dotfiles/nvim/.config/nvim/lua/plugin/vim_test.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/vim_test.lua
@@ -2,7 +2,6 @@ vim.g["test#strategy"] = "neovim"
 
 if vim.fn.executable "richgo" == 1 then
   vim.g["test#go#runner"] = "richgo"
-  vim.g["test#go#richgo#executable"] = "richgo test -v"
 end
 
 map("n", "<leader>l", "<Cmd>TestLast<CR>")
