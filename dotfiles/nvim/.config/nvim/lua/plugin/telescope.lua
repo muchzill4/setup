b45ba11a294bc3be2612ef0e7fe0b8fc281d1f90
Bartek Mucha <muchzill4@gmail.com>
2023-01-22 10:18:19 +0000

    `Gdiffsplit!` for merge conflicts

--- dotfiles/nvim/.config/nvim/lua/plugin/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/telescope.lua
@@ -77,7 +77,7 @@ telescope.setup {
         { name = "git branch", cmd = "Telescope git_branches" },
         { name = "git log", cmd = "Git log --oneline -100" },
         { name = "git buffer log", cmd = "Git log --oneline -100 %" },
-        { name = "git diff", cmd = "Gdiffsplit" },
+        { name = "git diff", cmd = "Gdiffsplit!" },
         { name = "git push", cmd = "Git push" },
         { name = "git push --force", cmd = "Git push --force" },
         {
