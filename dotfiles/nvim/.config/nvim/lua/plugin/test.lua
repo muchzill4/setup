e02eb829ed4f7a523ac370f62bae71cf9dd627ce
Bartek Mucha <muchzill4@gmail.com>
2023-02-08 15:05:48 +0000

    Dynamic test split size for smaller windows

--- dotfiles/nvim/.config/nvim/lua/plugin/test.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/test.lua
@@ -1,9 +1,23 @@
-vim.g["test#strategy"] = "neovim"
-vim.g["test#neovim#term_position"] = "bo 20"
+local function set_test_split_size()
+  print "setting term position"
+  local test_split_size = math.floor(vim.o.lines / 3)
+  if test_split_size < 20 then
+    test_split_size = math.floor(vim.o.lines / 2)
+  end
+  vim.g["test#neovim#term_position"] = "bo " .. test_split_size
+end
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = vim.api.nvim_create_augroup("SetTestSplitSize", {}),
+  callback = set_test_split_size,
+})
 
 if vim.fn.executable "richgo" == 1 then
   vim.g["test#go#runner"] = "richgo"
 end
+
+set_test_split_size()
+vim.g["test#strategy"] = "neovim"
 
 map("n", "<leader>l", "<Cmd>TestLast<CR>")
 map("n", "<leader>T", "<Cmd>TestFile<CR>")
