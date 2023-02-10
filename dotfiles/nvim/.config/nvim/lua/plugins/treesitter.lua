3d3b0f1954e7ac89698515f4d42e1bcbe56b209b
Bartek Mucha <muchzill4@gmail.com>
2023-02-10 20:09:43 +0000

    Common pattern for opts

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,62 +2,63 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    config = function()
-      local configs = require "nvim-treesitter.configs"
-      configs.setup {
-        ensure_installed = {
-          "bash",
-          "comment",
-          "fish",
-          "go",
-          "gomod",
-          "gowork",
-          "help",
-          "javascript",
-          "lua",
-          "make",
-          "markdown",
-          "python",
-          "yaml",
-        },
-        auto_install = true,
-        highlight = {
+    opts = {
+      ensure_installed = {
+        "bash",
+        "comment",
+        "fish",
+        "go",
+        "gomod",
+        "gowork",
+        "help",
+        "javascript",
+        "lua",
+        "make",
+        "markdown",
+        "python",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "gitcommit" },
+      },
+      indent = {
+        enable = true,
+        disable = { "yaml" },
+      },
+      textobjects = {
+        select = {
           enable = true,
-          additional_vim_regex_highlighting = { "gitcommit" },
-        },
-        indent = {
-          enable = true,
-          disable = { "yaml" },
-        },
-        textobjects = {
-          select = {
-            enable = true,
 
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
 
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
           },
         },
-      }
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
     end,
   },
 
