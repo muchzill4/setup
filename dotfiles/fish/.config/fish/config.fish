399d4e1fac3da98e771c13ce7e1f58dfa47ed5af
Bartek Mucha <muchzill4@gmail.com>
2023-03-20 13:28:17 +0000

    Update doubletrouble

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -12,11 +12,11 @@ if status is-interactive && ! functions --query fisher
   curl --silent --location https://git.io/fisher | source && fisher install jorgebucaran/fisher
 end
 
-# fish
-set fish_color_command 7FC79B --bold
+# colorscheme
+set fish_color_command 7FC79B
 set fish_color_error E25A6A
 set fish_color_normal CDCFE4
-set fish_color_operator ACAFD2
+set fish_color_operator 9C9FC9
 set fish_color_param normal
 set fish_color_quote D1AF9F
 set fish_color_search_match --background=342413
