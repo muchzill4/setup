91b88ffe62ee32b4a0d54e9758ffccb633d79340
Bartek Mucha <muchzill4@gmail.com>
2023-06-03 16:08:28 +0100

    Treesitter with `v`

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -30,10 +30,9 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+          init_selection = "v",
+          node_incremental = "v",
+          node_decremental = "V",
         },
       },
     },
