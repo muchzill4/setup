56e4df93fd034cfdafd58abd52ec0ece5265054f
Bartek Mucha <muchzill4@gmail.com>
2023-09-20 09:06:21 +0100

    Use default on_yank() timeout

--- dotfiles/nvim/.config/nvim/lua/mc4/options.lua
+++ dotfiles/nvim/.config/nvim/lua/mc4/options.lua
@@ -35,7 +35,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightOnYank", {}),
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank { timeout = 200 }
+    vim.highlight.on_yank()
   end,
 })
 
