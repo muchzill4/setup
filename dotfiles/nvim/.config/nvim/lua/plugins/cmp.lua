a6341682fcef26732791366ed06be8023b897bea
Bartek Mucha <muchzill4@gmail.com>
2023-09-21 18:05:45 +0100

    Disable cmp for cmdline and search - it's flaky

--- dotfiles/nvim/.config/nvim/lua/plugins/cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -73,21 +73,5 @@ return {
         },
       },
     }
-
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
-    })
   end,
 }
