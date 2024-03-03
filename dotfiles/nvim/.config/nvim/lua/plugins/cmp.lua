9e51d8e684d64d2f22c35a38828da455c8020e03
Bartek Mucha <muchzill4@gmail.com>
2024-03-03 19:01:10 +0000

    Predictable completion keys

--- dotfiles/nvim/.config/nvim/lua/plugins/cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -10,12 +10,6 @@ return {
     "dcampos/cmp-snippy",
   },
   config = function()
-    local function has_words_before()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0
-        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-    end
-
     local cmp = require "cmp"
     local snippy = require "snippy"
 
@@ -40,25 +34,15 @@ return {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
         },
-        ["<CR>"] = cmp.mapping.confirm { select = true },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif snippy.can_expand_or_advance() then
+        ["<C-y>"] = cmp.mapping.confirm { select = true },
+        ["<C-l>"] = cmp.mapping(function()
+          if snippy.can_expand_or_advance() then
             snippy.expand_or_advance()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif snippy.can_jump(-1) then
+        ["<C-h>"] = cmp.mapping(function()
+          if snippy.can_jump(-1) then
             snippy.previous()
-          else
-            fallback()
           end
         end, { "i", "s" }),
       },
