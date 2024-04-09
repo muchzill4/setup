283c3eaa2a807eba2c894bedb888806541102430
Bartek Mucha <muchzill4@gmail.com>
2024-04-09 18:12:15 +0100

    Colorscheme as per lazy README

--- dotfiles/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,9 @@
 return {
   dir = "~/Dev/my/doubletrouble",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000,
   config = function()
-    vim.cmd "colorscheme doubletrouble"
     vim.opt.termguicolors = true
+    vim.cmd "colorscheme doubletrouble"
   end,
 }
