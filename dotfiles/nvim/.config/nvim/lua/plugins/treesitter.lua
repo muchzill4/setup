763cea1ebb819d2361a8d9574c7269ec235e8fbb
Bartek Mucha <muchzill4@gmail.com>
2024-12-20 07:52:53 +0000

    Simplify treesitter setup

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,36 +5,13 @@ return {
     main = "nvim-treesitter.configs",
     opts = {
       ensure_installed = {
-        "bash",
         "comment",
-        "fish",
-        "go",
-        "gomod",
-        "gowork",
-        "javascript",
-        "lua",
-        "make",
         "markdown",
-        "python",
-        "yaml",
-        "vimdoc",
+        "markdown_inline",
       },
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { "gitcommit" },
-      },
-      indent = {
-        enable = true,
-        disable = { "yaml" },
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "v",
-          node_incremental = "v",
-          node_decremental = "V",
-        },
       },
     },
   },
