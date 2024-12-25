557a591ced8db147bf57c8058632d9637cc4e868
Bartek Mucha <muchzill4@gmail.com>
2024-12-25 21:31:06 +0000

    Explicit parser list

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,11 +5,22 @@ return {
     main = "nvim-treesitter.configs",
     opts = {
       ensure_installed = {
+        "bash",
         "comment",
+        "fish",
+        "go",
+        "gomod",
+        "gowork",
+        "javascript",
+        "lua",
+        "make",
         "markdown",
         "markdown_inline",
+        "python",
+        "vimdoc",
+        "yaml",
       },
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
       },
