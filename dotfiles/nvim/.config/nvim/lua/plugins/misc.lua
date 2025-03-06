2ca6f7ea5798f092d81915cea492b7db08b8ed8d
Bartek Mucha <muchzill4@gmail.com>
2025-03-06 10:58:23 +0000

    Tmux again

--- dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
@@ -66,9 +66,22 @@ return {
   { "echasnovski/mini.surround", version = false, config = true }, -- fast and feature-rich surround actions
 
   {
-    "knubie/vim-kitty-navigator",
-    build = { "cp ./*.py ~/.config/kitty/" },
-    init = function() vim.g["kitty_navigator_enable_stack_layout"] = 1 end,
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   },
 
   {
