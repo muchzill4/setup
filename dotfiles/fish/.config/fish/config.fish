8054240a1ae883c735f6cc48de4f41efeec11f85
Bartek Mucha <muchzill4@gmail.com>
2025-03-26 21:03:28 +0000

    In yo face fish search

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -19,7 +19,7 @@ set fish_color_normal CDCFE4
 set fish_color_operator 9C9FC9
 set fish_color_param normal
 set fish_color_quote D1AF9F
-set fish_color_search_match --background=342413
+set fish_color_search_match 1D1D30 --background=E6A64C
 set fish_color_valid_path normal --underline
 set fish_pager_color_progress black
 
