6fe0185ed9a84361f2019c6850e1b55212855784
Bartek Mucha <muchzill4@gmail.com>
2025-03-27 12:27:55 +0000

    Drop too much gitsigns config

--- dotfiles/nvim/.config/nvim/lua/plugins/git.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/git.lua
@@ -38,13 +38,6 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
         local opts = { buffer = bufnr, expr = true }
