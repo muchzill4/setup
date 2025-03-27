2bffe54638439a1b16e743fa67be53c0af039085
Bartek Mucha <muchzill4@gmail.com>
2025-03-27 20:40:31 +0000

    LspRestart

--- dotfiles/nvim/.config/nvim/lua/mc4/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/mc4/lsp.lua
@@ -52,3 +52,30 @@ vim.api.nvim_create_autocmd("LspDetach", {
 })
 
 vim.lsp.enable { "elixirls", "luals", "basedpyright", "ruff", "css", "html", "gopls", "tsls" }
+
+local api, lsp = vim.api, vim.lsp
+local get_clients = vim.lsp.get_clients
+
+local function client_complete()
+  --- @param c vim.lsp.Client
+  --- @return string
+  return vim.tbl_map(function(c) return c.name end, get_clients())
+end
+
+api.nvim_create_user_command("LspRestart", function(kwargs)
+  local name = kwargs.fargs[1] --- @type string
+  for _, client in ipairs(get_clients { name = name }) do
+    local bufs = vim.deepcopy(client.attached_buffers)
+    client:stop()
+    vim.wait(30000, function() return lsp.get_client_by_id(client.id) == nil end)
+    local client_id = lsp.start(client.config)
+    if client_id then
+      for buf in pairs(bufs) do
+        lsp.buf_attach_client(buf, client_id)
+      end
+    end
+  end
+end, {
+  nargs = "*",
+  complete = client_complete,
+})
