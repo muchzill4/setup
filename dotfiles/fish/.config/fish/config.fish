754e86264005842e3f07eefd51288967628ea00a
Bartek Mucha <muchzill4@gmail.com>
2025-04-30 07:40:57 +0100

    Update colorscheme

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -19,9 +19,9 @@ set fish_color_normal CDCFE4
 set fish_color_operator 9C9FC9
 set fish_color_param normal
 set fish_color_quote D1AF9F
-set fish_color_search_match 1D1D30 --background=E6A64C
+set fish_color_search_match 191929 --background=E6A64C
 set fish_color_valid_path normal --underline
-set fish_pager_color_progress black
+set fish_pager_color_progress 565B8F
 
 # pure
 set pure_shorten_prompt_current_directory_length 1
