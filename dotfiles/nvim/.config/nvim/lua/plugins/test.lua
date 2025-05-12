dd5650fd3e5b477eede543a45264c41bb70fc04d
Bartek Mucha <muchzill4@gmail.com>
2025-05-12 12:09:54 +0100

    Improve test buffer navigation

--- dotfiles/nvim/.config/nvim/lua/plugins/test.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/test.lua
@@ -1,41 +1,68 @@
 local test_runs = {
-  { cmd = "cargo test", failure_pattern = "^----.*----$" },
-  { cmd = "go test", failure_pattern = "FAIL:.*" },
-  { cmd = "gotestsum", failure_pattern = "FAIL:.*" },
+  {
+    cmd = "cargo test",
+    failing_test_pattern = "^----.*----$",
+  },
+  {
+    cmd = "go test",
+    failing_test_pattern = "FAIL:.*",
+  },
+  {
+    cmd = "gotestsum",
+    failing_test_pattern = "FAIL:.*",
+  },
   { cmd = ".bin/jest" },
-  { cmd = "pytest", failure_pattern = [[^_\+ .* _\+$]] },
-  { cmd = "mix test", failure_pattern = [[^\s\+\d\+) test.*$]] },
+  {
+    cmd = "pytest",
+    failing_test_pattern = [[^_\+ .* _\+$]],
+  },
+  {
+    cmd = "mix test",
+    failing_test_pattern = [[^\s\+\d\+) test.*$]],
+    file_location_pattern = [[\s\+\zs.*\.exs:\d\+]],
+  },
 }
 
-local function get_test_buf_info()
-  local buf_name = vim.api.nvim_buf_get_name(0)
+local function get_test_run_info(buffer)
+  local buf_name = vim.api.nvim_buf_get_name(buffer)
   for _, t in ipairs(test_runs) do
     if string.find(buf_name, t.cmd) then
-      return vim.tbl_extend("error", t, { is_success = vim.v.event["status"] == 0 })
+      return vim.tbl_extend("error", t, { has_failing_tests = vim.v.event["status"] ~= 0 })
     end
   end
 end
 
-local function focus_first_match(search_pattern)
-  if type(search_pattern) == "table" then
-    search_pattern = table.concat(search_pattern, "\\|")
-  end
-  vim.fn.setreg("/", search_pattern)
-
-  local escaped_keys = vim.api.nvim_replace_termcodes("<C-\\><C-n>n", true, true, true)
+local function focus_first_match(pattern)
+  local escaped_keys = vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, true, true)
   vim.api.nvim_feedkeys(escaped_keys, "n", true)
-  vim.defer_fn(function() vim.cmd "silent! nohl" end, 10) -- 10ms delay
+  vim.defer_fn(function() vim.fn.search(pattern) end, 10)
+end
+
+local function setup_navigation_shortcuts(buffer, test_run_info)
+  local opts = { noremap = true, silent = true, buffer = buffer }
+  if test_run_info.failing_test_pattern then
+    map("n", "]t", function() vim.fn.search(test_run_info.failing_test_pattern) end, opts)
+    map("n", "[t", function() vim.fn.search(test_run_info.failing_test_pattern, "b") end, opts)
+  end
+  if test_run_info.file_location_pattern then
+    map("n", "]f", function() vim.fn.search(test_run_info.file_location_pattern) end, opts)
+    map("n", "[f", function() vim.fn.search(test_run_info.file_location_pattern, "b") end, opts)
+  end
 end
 
 vim.api.nvim_create_autocmd("TermClose", {
   group = vim.api.nvim_create_augroup("TestRunAutocloseOrFocusFirstFailure", {}),
-  pattern = "*",
-  callback = function()
-    local test_buf_info = get_test_buf_info()
-    if test_buf_info == nil or test_buf_info.is_success then
+  pattern = "term://*",
+  callback = function(args)
+    local test_run_info = get_test_run_info(args.buf)
+    if test_run_info == nil then
       return
     end
-    focus_first_match(test_buf_info.failure_pattern)
+
+    if test_run_info.has_failing_tests then
+      focus_first_match(test_run_info.failing_test_pattern)
+      setup_navigation_shortcuts(args.buf, test_run_info)
+    end
   end,
 })
 
