711d5b4f68aa5bab9df553b233c8728f6211068f
Bartek Mucha <muchzill4@gmail.com>
2025-07-30 22:15:06 +0100

    Add session name to tab bar

---
+++ dotfiles/kitty/.config/kitty/tab_bar.py
@@ -0,0 +1,50 @@
+from kitty.fast_data_types import Screen, get_boss, current_os_window
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    TabBarData,
+    draw_title,
+    as_rgb,
+    color_as_int,
+    draw_tab_with_slant,
+)
+
+
+def draw_right_status(
+    draw_data: DrawData,
+    screen: Screen,
+    is_last: bool,
+) -> int:
+    if not is_last:
+        return screen.cursor.x
+
+    id = current_os_window()
+    boss = get_boss()
+    name = boss.os_window_map.get(id).wm_name
+    right_status = f"{name} "
+    right_status_length = len(right_status)
+
+    screen.cursor.x = max(screen.cursor.x, screen.columns - right_status_length)
+    screen.cursor.bg = 0  # (draw_data.default_bg)
+    screen.cursor.bold = False
+    screen.draw(right_status)
+    return screen.cursor.x
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_tab_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    x = draw_tab_with_slant(
+        draw_data, screen, tab, before, max_tab_length, index, is_last, extra_data
+    )
+
+    draw_right_status(draw_data, screen, is_last)
+
+    return x
