f01a3340f36a5868c712f51fc635282df4072379
Bartek Mucha <muchzill4@gmail.com>
2025-07-30 10:51:24 +0100

    Deprecate tmux

--- dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
@@ -71,22 +71,9 @@ return {
   },
 
   {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
+    "knubie/vim-kitty-navigator",
+    build = { "cp ./*.py ~/.config/kitty/" },
+    init = function() vim.g["kitty_navigator_enable_stack_layout"] = 1 end,
   },
 
   {
