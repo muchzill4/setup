8339c7fd3bbcdaf652713108a84dcbea93e4331b
Bartek Mucha <muchzill4@gmail.com>
2025-07-31 10:10:59 +0100

    Kovid confirmed this is the way

--- dotfiles/kitty/.config/kitty/tab_bar.py
+++ dotfiles/kitty/.config/kitty/tab_bar.py
@@ -10,19 +10,11 @@ from kitty.tab_bar import (
 
 
 def is_same_tab(drawn_tab: TabBarData, os_window_tab: TabDict) -> bool:
-    return (
-        os_window_tab["id"] == drawn_tab.tab_id
-        and os_window_tab["title"] == drawn_tab.title
-    )
+    return os_window_tab["id"] == drawn_tab.tab_id
 
 
-# This is a bit jank, but it seems kitty doesn't expose the information about what os window we're drawing the tab bar for.
-# So, the only way to identify the os window the tab bar is drawn for is to compare its tabs.
-# Here, I'm only using the last tab to find a matching os window.
 def find_matching_os_window(last_tab: TabBarData) -> OSWindowDict:
-    boss = get_boss()
-    os_windows = boss.list_os_windows()
-    for os_window in os_windows:
+    for os_window in get_boss().list_os_windows():
         last_os_window_tab = os_window["tabs"][-1]
         if is_same_tab(last_tab, last_os_window_tab):
             return os_window
@@ -33,10 +25,8 @@ def draw_right_status(
     screen: Screen,
 ):
     os_window = find_matching_os_window(last_tab)
-
     right_status = f"{os_window['wm_name']} "
     right_status_length = len(right_status)
-
     screen.cursor.x = max(screen.cursor.x, screen.columns - right_status_length)
     screen.cursor.bg = 0
     screen.cursor.bold = False
