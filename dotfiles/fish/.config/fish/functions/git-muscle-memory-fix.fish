a86847d4b9d1c03a259dcd6c7d7e44b83e29b145
Bartek Mucha <muchzill4@gmail.com>
2025-11-11 15:34:41 +0000

    I no longer care about this

--- dotfiles/fish/.config/fish/functions/git-muscle-memory-fix.fish
+++
@@ -1,8 +0,0 @@
-function git-muscle-memory-fix -d "Alias main branch to master branch"
-  set -l main (git branch --list main)
-
-  if test -n "$main"
-    git symbolic-ref refs/remotes/origin/master refs/remotes/origin/main &&
-    git symbolic-ref refs/heads/master refs/heads/main
-  end
-end
