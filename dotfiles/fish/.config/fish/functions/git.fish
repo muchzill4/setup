2395a36560b4b9111979234ed50d53c9ebc0e3d2
Bartek Mucha <muchzill4@gmail.com>
2025-11-13 09:47:59 +0000

    Git/GH idioms vs custom scripts

--- dotfiles/fish/.config/fish/functions/git.fish
+++ dotfiles/fish/.config/fish/functions/git.fish
@@ -4,10 +4,28 @@ function git
     echo "🚫 checkout is banned! Use modern Git commands:"
     set_color normal
     echo "💡 git switch <branch>"
-    echo "💡 git switch -c <new-branch>" 
+    echo "💡 git switch -c <new-branch>"
     echo "💡 git switch --detach <sha>"
     echo "💡 git restore <file>"
     return 1
+  else if test "$argv[1]" = "switch"; and test (count $argv) -eq 1
+    set -l branch (
+      command git branch |
+      rg -v '\*' |
+      cut -c 3- |
+      fzf --prompt "Checkout: "
+    )
+    test -z "$branch"; and return 1
+    command git switch "$branch"
+  else if test "$argv[1]" = "branch"; and test (count $argv) -eq 2; and test "$argv[2]" = "-D"
+    set -l branch (
+      command git branch |
+      rg -v '\*' |
+      cut -c 3- |
+      fzf --prompt "Delete branch: "
+    )
+    test -z "$branch"; and return 1
+    command git branch -D "$branch"
   else
     command git $argv
   end
