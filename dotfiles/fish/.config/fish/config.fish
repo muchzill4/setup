18cfd60a989211d9648f51db3e769039c0b72fa4
Bartek Mucha <muchzill4@gmail.com>
2026-03-15 15:14:39 +0000

    Sync fish theme

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,9 +2,15 @@
 set -g fish_greeting
 
 # colorscheme
-set fish_color_search_match 191929 --background=E6A64C
+set fish_color_command 7FC79B
+set fish_color_cwd 6CA9EF
+set fish_color_error E25A6A
 set fish_color_param BB781B
 set fish_color_quote D1AF9F
+set fish_color_search_match 191929 --background=E6A64C
+set fish_color_operator 9C9FC9
+set fish_color_end 9C9FC9
+set fish_color_redirection 9C9FC9 --bold
 
 # hydro
 set hydro_color_pwd blue
