bd50a5b78df020c02829f0c35b7e66680ab7d890
Bartek Mucha <muchzill4@gmail.com>
2026-03-26 09:13:19 +0000

    Bump plugs

--- dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
@@ -72,7 +72,6 @@ return {
 
   {
     "knubie/vim-kitty-navigator",
-    build = { "cp ./*.py ~/.config/kitty/" },
     init = function() vim.g["kitty_navigator_enable_stack_layout"] = 1 end,
   },
 
