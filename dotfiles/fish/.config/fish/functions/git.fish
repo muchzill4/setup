0a8b365ee88cdc3a196ecc1ca74c9d5500bbf431
Bartek Mucha <muchzill4@gmail.com>
2026-04-08 14:08:51 +0100

    Add fzf picker with branch delete to git switch

--- dotfiles/fish/.config/fish/functions/git.fish
+++ dotfiles/fish/.config/fish/functions/git.fish
@@ -9,17 +9,34 @@ function git
     echo "💡 git restore <file>"
     return 1
   else if test "$argv[1]" = "switch"; and test (count $argv) -eq 1
-    command git branch |
-      rg -v '\*' |
-      cut -c 3- |
-      fzf --prompt "Checkout: " |
-      xargs -r git switch
-  else if test "$argv[1]" = "branch"; and test (count $argv) -eq 2; and test "$argv[2]" = "-D"
-    command git branch |
-      rg -v '\*' |
-      cut -c 3- |
-      fzf --multi --prompt "Delete branches: " |
-      xargs -r git branch -D
+    set -l result (
+      command git branch |
+        rg -v '\*' |
+        cut -c 3- |
+        fzf --cycle --layout=reverse --multi --expect=alt-x \
+          --header="enter: switch | alt-x: delete | tab: mark"
+    )
+
+    test -n "$result"; or return 0
+
+    set -l key $result[1]
+    set -l selected $result[2..]
+
+    test -n "$selected"; or return 0
+
+    switch "$key"
+      case alt-x
+        printf 'Delete branches:\n'
+        printf '  %s\n' $selected
+        read -l -P 'Confirm? [y/N] ' confirm
+        test "$confirm" = "y"; or return 0
+
+        for branch in $selected
+          command git branch -D "$branch"
+        end
+      case '*'
+        command git switch "$selected[1]"
+    end
   else
     command git $argv
   end
