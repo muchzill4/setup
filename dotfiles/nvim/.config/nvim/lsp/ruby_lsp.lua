cdc6ea5a0080eda0871145389abc7a12ae2890cb
Bartek Mucha <muchzill4@gmail.com>
2026-09-02 13:11:42 +0100

    Drop some lsp

--- dotfiles/nvim/.config/nvim/lsp/ruby_lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  cmd = { "ruby-lsp" },
-  filetypes = { "ruby", "eruby" },
-  root_markers = { ".git", "Gemfile" },
-  init_options = {
-    formatter = "auto",
-  },
-  single_file_support = true,
-}
